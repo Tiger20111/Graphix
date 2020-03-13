36bdc31157e539b5db6e4305ef0dfba08efe163a
Tiger20111 <nikit-proc@mail.ru>
2020-03-14 01:29:21 +0300

    add homework1

---
+++ hm1/two-triangles.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/tiger/Documents/hm12/ogl-master/tutorial02_red_triangle/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\tiger\Documents\hm12\build\Debug\tutorial02_red_triangle.exe"  
+pause
